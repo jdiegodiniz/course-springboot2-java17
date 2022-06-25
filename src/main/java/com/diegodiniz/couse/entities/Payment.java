c26f6777dbdb908483e8931db44ada755e182f24
Diego Diniz <diegodiniz90@hotmail.com>
2022-06-25 18:48:54 -0300

    Subtotal & Total methods

--- src/main/java/com/diegodiniz/couse/entities/Payment.java
+++ src/main/java/com/diegodiniz/couse/entities/Payment.java
@@ -1,5 +1,7 @@
 package com.diegodiniz.couse.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.*;
 import java.io.Serializable;
 import java.time.Instant;
@@ -11,6 +13,7 @@ public class Payment implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private Instant moment;
+    @JsonIgnore
     @OneToOne
     @MapsId
     private Order order;
