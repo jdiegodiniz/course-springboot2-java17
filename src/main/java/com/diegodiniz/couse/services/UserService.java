521b7e934ede8b80754214b8a583a830ccf798f3
Diego Diniz <diegodiniz90@hotmail.com>
2022-06-25 20:02:39 -0300

    Exception handling - delete

--- src/main/java/com/diegodiniz/couse/services/UserService.java
+++ src/main/java/com/diegodiniz/couse/services/UserService.java
@@ -2,9 +2,12 @@ package com.diegodiniz.couse.services;
 
 import com.diegodiniz.couse.entities.User;
 import com.diegodiniz.couse.repositories.UserRepository;
+import com.diegodiniz.couse.services.exceptions.DatabaseException;
 import com.diegodiniz.couse.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -29,7 +32,15 @@ public class UserService {
     }
 
     public void delete(Long id){
-        repository.deleteById(id);
+        try {
+            repository.deleteById(id);
+        }
+        catch (EmptyResultDataAccessException e){
+            throw new ResourceNotFoundException(id);
+        }
+        catch (DataIntegrityViolationException e){
+            throw new DatabaseException(e.getMessage());
+        }
     }
 
     public User update(Long id, User obj){
