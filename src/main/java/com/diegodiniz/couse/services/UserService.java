243479a7f35bd36281e6f51ddba18265188a20a2
Diego Diniz <diegodiniz90@hotmail.com>
2022-06-25 09:32:12 -0300

    Service layer, Component registration

---
+++ src/main/java/com/diegodiniz/couse/services/UserService.java
@@ -0,0 +1,25 @@
+package com.diegodiniz.couse.services;
+
+import com.diegodiniz.couse.entities.User;
+import com.diegodiniz.couse.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll(){
+        return repository.findAll();
+    }
+
+    public User findById(Long id){
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+}
