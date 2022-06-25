37428317e423ae2439ecaac4548d76506b749650
Diego Diniz <diegodiniz90@hotmail.com>
2022-06-25 11:26:49 -0300

    Many-to-Many association with Join Table

--- src/main/java/com/diegodiniz/couse/entities/Category.java
+++ src/main/java/com/diegodiniz/couse/entities/Category.java
@@ -1,5 +1,7 @@
 package com.diegodiniz.couse.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.HashSet;
@@ -13,7 +15,8 @@ public class Category implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
-    @Transient
+    @JsonIgnore
+    @ManyToMany(mappedBy = "categories")
     private Set<Product> products = new HashSet<>();
 
     public Category(){
