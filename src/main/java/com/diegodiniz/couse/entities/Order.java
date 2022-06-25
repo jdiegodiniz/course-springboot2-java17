86c8cab76d30cedb136177dc78c1a21863938447
Diego Diniz <diegodiniz90@hotmail.com>
2022-06-25 10:33:26 -0300

    OrderStatus ENUM

--- src/main/java/com/diegodiniz/couse/entities/Order.java
+++ src/main/java/com/diegodiniz/couse/entities/Order.java
@@ -1,7 +1,8 @@
 package com.diegodiniz.couse.entities;
 
+import com.diegodiniz.couse.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonIgnore;
+
 
 import javax.persistence.*;
 import java.io.Serializable;
@@ -15,6 +16,7 @@ public class Order implements Serializable {
     private Long id;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
+    private Integer orderStatus;
 
     @ManyToOne
     @JoinColumn(name = "client_id")
@@ -23,10 +25,12 @@ public class Order implements Serializable {
     public Order(){
     }
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
+
     }
 
     public Long getId() {
@@ -43,6 +47,16 @@ public class Order implements Serializable {
 
     public void setMoment(Instant moment) {
         this.moment = moment;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return  OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if(orderStatus !=null) {
+            this.orderStatus = orderStatus.getCode();
+        }
     }
 
     public User getClient() {
